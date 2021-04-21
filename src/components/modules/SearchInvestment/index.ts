e05fc9ae936db69635e202499853cd4db052547d
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-04-21 19:33:28 +0700

    refactor: Move investment search to separate component

---
+++ src/components/modules/SearchInvestment/index.ts
@@ -0,0 +1,1 @@
+export { default as SearchInvesment } from './SearchInvestment';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,134 +1,12 @@
 import * as React from 'react';
 
 import Head from 'next/head';
-import useSWR from 'swr';
 
-import { gql } from 'graphql-request';
+import { Heading, Text, Container, Box } from '@chakra-ui/react';
 
-import { Heading, Text, Container, Box, Flex, Button, Input } from '@chakra-ui/react';
-
-import { AutoComplete } from '@/components/elements/AutoComplete';
-import { Error } from '@/components/elements/Error';
-import { Highlight } from '@/components/elements/Highlight';
-
-import { Illegal, App } from '@/common/types';
-
-import { useDebounce } from '@/hooks/useDebounce';
-import { graphQLFetcher } from '@/utils/fetcher';
-
-type GraphQLResult = {
-  illegalInvestments: Illegal[];
-  apps: App[];
-};
+import { SearchInvesment } from '@/components/modules/SearchInvestment';
 
 function Home() {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const [debouncedSearchTerm, setDebouncedSearchTerm] = React.useState('');
-
-  const handleInput = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const debouncedSetter = useDebounce(setDebouncedSearchTerm, 250);
-
-  const { data, error } = useSWR<GraphQLResult, any>(
-    () => {
-      return gql`query {
-        illegalInvestments(name: "${debouncedSearchTerm}", limit: 5) {
-          name
-        }
-        apps(name: "${debouncedSearchTerm}", limit: 5) {
-          name
-        }
-      }`;
-    },
-    graphQLFetcher,
-  );
-
-  React.useEffect(() => {
-    if (searchTerm.length > 1) {
-      debouncedSetter(searchTerm);
-    } else if (!searchTerm) {
-      debouncedSetter('');
-    }
-  }, [searchTerm]);
-
-  const handleSuggestionSelect = (name: string) => setSearchTerm(name);
-
-  const suggest = React.useCallback(() => {
-    if (!debouncedSearchTerm) {
-      return <></>;
-    }
-
-    if (!data) {
-      return (
-        <AutoComplete.SuggestionsContainer
-          as="ul"
-          maxHeight={64}
-          margin={2}>
-          <AutoComplete.SuggestionSkeleton />
-          <AutoComplete.SuggestionSkeleton />
-          <AutoComplete.SuggestionSkeleton />
-        </AutoComplete.SuggestionsContainer>
-      );
-    }
-
-    if (error) {
-      return (
-        <Error
-          marginTop={4}
-          dismissable={true}>
-          {error}
-        </Error>
-      );
-    }
-
-    return (
-      <AutoComplete.SuggestionsContainer
-        as="ul"
-        maxHeight={64}
-        margin={2}>
-        {mapInvestmentData()}
-      </AutoComplete.SuggestionsContainer>
-    );
-  }, [debouncedSearchTerm, data, error]);
-
-  const mapInvestmentData = () => {
-    const { illegalInvestments, apps } = data;
-
-    if (!illegalInvestments.length && !apps.length) {
-      return <AutoComplete.EmptySuggestion />;
-    }
-
-    const elem: JSX.Element[] = [];
-    const suggestions = [...illegalInvestments, ...apps];
-
-    suggestions.sort((a, b) => {
-      if (a.name < b.name) {
-        return -1;
-      } else if (a.name > b.name) {
-        return 1;
-      }
-
-      return 0;
-    });
-
-    suggestions.forEach(({ name }, index) => {
-      elem.push(
-        <AutoComplete.Suggestion
-          key={index}
-          index={index}
-          onClick={() => handleSuggestionSelect(name)}>
-          <Text maxW="100%" isTruncated={true}>
-            <Highlight text={name} term={searchTerm} />
-          </Text>
-        </AutoComplete.Suggestion>,
-      );
-    });
-
-    return elem;
-  };
-
   return (
     <>
       <Head>
@@ -157,39 +35,7 @@ function Home() {
           </Text>
         </Box>
 
-        <Flex
-          justifyContent="space-between"
-          paddingX={36}>
-          <AutoComplete>
-            <Box w="full">
-              <AutoComplete.Input>
-                <Input
-                  autoComplete="false"
-                  value={searchTerm}
-                  onInput={handleInput}
-                  focusBorderColor="primary.lighten"
-                  placeholder="Cari investasi atau perusahaan"
-                  size="lg"
-                  type="text" />
-              </AutoComplete.Input>
-              {suggest()}
-            </Box>
-          </AutoComplete>
-
-          <Button
-            ml={4}
-            px={8}
-            size="lg"
-            bg="primary.base"
-            color="white"
-            fontWeight={500}
-            letterSpacing={0.5}
-            _hover={{
-              backgroundColor: 'primary.darken',
-            }}>
-            Periksa
-          </Button>
-        </Flex>
+        <SearchInvesment />
       </Container>
     </>
   );
