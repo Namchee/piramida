a53839036747968a11d5389e602aadec750044f0
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-05-21 21:32:11 +0700

    :art: refactor: Add better styling to JSON Box

--- src/components/elements/JSONBox/JSONBox.tsx
+++ src/components/elements/JSONBox/JSONBox.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import Prism from 'prismjs';
 import 'prismjs/components/prism-json';
-import 'prismjs/themes/prism-tomorrow.css';
+import 'prism-themes/themes/prism-atom-dark.css';
 
 function JSONBox({ children }: React.PropsWithChildren<{}>) {
   const codeBlock = React.useRef(null);
