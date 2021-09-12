0f4a8563e2a3355a5dcb00977c1a374462cba955
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-09-12 21:29:35 +0700

    [WIP] refactor: Migrate to atkinson hyperlegible

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const { theme } = require('tailwindcss/defaultConfig');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Atkinson Hyperlegible', ...theme.fontFamily.sans],
+    },
     extend: {
       colors: {
         'primary': '#185ADB',
