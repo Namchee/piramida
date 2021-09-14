ef1e40d044b3eb60ab4e036eace6ef14a43ba7d8
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-09-14 20:05:52 +0700

    :hammer: chore: Introduce eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,13 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true,
-  },
-  'extends': [
-    'plugin:react/recommended',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
     'google',
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-    },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-  ],
-  'rules': {
-    'linebreak-style': [
-      'warn',
-      'windows',
-    ],
-    'indent': [
-      'error',
-      2,
-      { 'SwitchCase': 1 },
-    ],
-    'max-len': 'off',
-    'object-curly-spacing': [
-      'error',
-      'always',
-    ],
-    'require-jsdoc': 'off',
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+    'object-curly-spacing': ['error', 'always'],
   },
 };
