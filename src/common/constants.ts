ccc9b64d738376dd1013aa3f0393ab8a2ad4ad68
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-10-13 19:34:25 +0700

    :art: style: add gray 400 as placeholder color

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,3 @@
 // other than this is insanity
 export const API_DATE_FORMAT = 'DD/MM/YYYY';
 export const DATE_FORMAT = 'D MMMM YYYY';
-
-// favicon size on search result
-export const FAVICON_SIZE = 32;
